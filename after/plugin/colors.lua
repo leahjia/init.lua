3bcb1cc1135b32864f5829cbee08890fded02216
Leah Jia <yuwenj3@uw.edu>
2023-08-19 18:39:02 -0700

    more mappings

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -6,8 +6,8 @@ function Theme(color)
 	color = color or "onedark"
 	vim.cmd.colorscheme(color)
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "#1E2127" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1E2127" })
+	-- vim.api.nvim_set_hl(0, "Normal", { bg = "#1E2127" })
+	-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1E2127" })
 end
 
 Theme()
--- lua/leah/remap.lua
+++ lua/leah/remap.lua
@@ -1,2 +1,22 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("v", "<C-j>", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "<C-k>", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+vim.keymap.set("i", "<C-c>", "<Esc>")
+
+vim.keymap.set("n", "Q", "<nop>")
+
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+
+vim.keymap.set("n", "<leader>f", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- set executable file
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
