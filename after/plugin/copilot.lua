69d0c3fb7e820a72112739684d005f86366321c2
Leah Jia <99011487+leahjia@users.noreply.github.com>
2023-12-02 17:25:21 -0800

    toggle

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -1,10 +1,22 @@
 vim.g.copilot_no_tab_map = true
 vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
 
-vim.g.copilot_filetypes = {
-    -- ["*"] = false,
-    ["java"] = false,
-    ["rust"] = false,
-    ["python"] = false,
-}
+-- vim.g.copilot_filetypes = {
+    -- ["java"] = false,
+    -- ["rust"] = false,
+    -- ["python"] = false,
+    -- }
+
+
+vim.api.nvim_create_user_command(
+    'ToggleCopilot',
+    function()
+        local copilot_enabled = vim.g.copilot_enabled or false
+        vim.g.copilot_enabled = not copilot_enabled
+        print("Copilot: " .. tostring(vim.g.copilot_enabled))
+
+        vim.g.copilot_filetypes = { ["*"] = vim.g.copilot_enabled }
+    end,
+    {}
+)
 
--- lua/leah/remap.lua
+++ lua/leah/remap.lua
@@ -37,3 +37,5 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 
 -- set executable file
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+vim.keymap.set("n", "<C-S-c>", ":ToggleCopilot<CR>", { silent = true })
