3c7fe588c0bf8c5fa9d3034f1cd2d2c5a993dd5b
Leah Jia <99011487+leahjia@users.noreply.github.com>
2024-04-25 10:03:29 -0700

    hl yanked txt

--- lua/leah/set.lua
+++ lua/leah/set.lua
@@ -42,3 +42,10 @@ vim.api.nvim_create_autocmd("FileType", {
     end,
 })
 
+vim.api.nvim_create_autocmd("TextYankPost", {
+    desc = "highlight yanked text",
+    group = vim.api.nvim_create_augroup("HighlightYank", { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+})
